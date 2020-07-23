3e45245691c181c1a1004490c74e2dfdc1c10dbe
Andrea Simone Costa <andrysimo1997@gmail.com>
2020-07-23 23:15:59 +0200

    add ConcatMap

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <ConcatMapTest />
+</template>
+
+<script>
+import ConcatMapTest from './components/ConcatMap/ConcatMapTest.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ConcatMapTest
+  }
+}
+</script>
